1dbb1f9c9f4aabe37560ebdfce57b1e72974fe2f
lyx# <71846446+dr3adlyx@users.noreply.github.com>
2023-02-17 14:31:54 +0100

    Version: 1_102

---
+++ EdgeRemover/README_BEFORE_RUNNING.bat
@@ -0,0 +1,6 @@
+# This is an Edge Killer.
+
+When i downloaded the file myself on another device, Defender stated that it may be malicious. **IT IS NOT**.
+
+Well, anyways, here is some information you may or may not need.
+
