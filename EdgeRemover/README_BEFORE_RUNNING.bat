219f4d9d9a150617da3d2a0f657b1aca114abb68
lyx# <71846446+dr3adlyx@users.noreply.github.com>
2023-02-17 17:03:03 +0100

    Update README_BEFORE_RUNNING.bat

--- EdgeRemover/README_BEFORE_RUNNING.bat
+++ EdgeRemover/README_BEFORE_RUNNING.bat
@@ -4,3 +4,5 @@ When i downloaded the file myself on another device, Defender stated that it may
 
 Well, anyways, here is some information you may or may not need.
 
+If you are using it without looking into the source code, currently only Version "110.0.1587.49" is working.
+(Obviously, you could just change Line 12 to whatever Version your Edge is running on, but thats h4x0r stuff.
